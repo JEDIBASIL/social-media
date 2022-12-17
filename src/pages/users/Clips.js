0a14ef5176ec96098a3be56714b2c9658dc84293
JEDIBASIL <jedidiahbasil@gmail.com>
2022-12-17 01:27:38 +0100

    added custom controls for clip videos and made the clip page responsive

--- src/pages/users/Clips.js
+++ src/pages/users/Clips.js
@@ -1,36 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useRef } from 'react';
-import { HiHeart, HiChat, HiArrowUp,HiArrowDown, HiPlay, HiOutlineVolumeUp } from "react-icons/hi"
+import { RiVolumeUpFill, RiVolumeMuteFill, RiHeartFill, RiChatQuoteFill, RiPlayFill, RiPauseFill } from "react-icons/ri"
 import pics2 from "../../static/images/anime.jpg";
 import vid from "../../static/images/vid.mp4"
 const Clips = () => {
     const clipVid = useRef(null)
     const [clipVidControls, setClipVidControls] = useState({
-        muted:true,
-        playing:false
+        muted: true,
+        playing: true
     })
-   const onPauseToggle = () => {
-    setClipVidControls({...clipVidControls, playing:true})
-    clipVidControls.playing ? clipVid.current.play() : clipVid.current.pause()
-    console.log(clipVidControls)
-    }
-
-    const onMutedToggle = (e) =>{
-        setClipVidControls({muted:this.muted, ...clipVidControls})
+    const onPauseToggle = () => {
+        setClipVidControls({ ...clipVidControls, playing: !clipVidControls.playing })
+        clipVidControls.playing ? clipVid.current.play() : clipVid.current.pause()
         console.log(clipVidControls)
     }
-    useEffect(()=>{
-        console.log(clipVid.current)
-        clipVid.current.play()
 
-    })
+    const onMutedToggle = (e) => {
+        setClipVidControls({ ...clipVidControls, muted: !clipVidControls.muted })
+    }
+
     console.log(vid)
     return (
         <div className='clip'>
             <div className='clipCard'>
                 <div className='vid_controls'>
-                <HiPlay onClick={onPauseToggle}  />
-                <HiOutlineVolumeUp onClick={onMutedToggle} />
+                    <span onClick={onPauseToggle}>
+                        {!clipVidControls.playing ? <RiPlayFill />: <RiPauseFill />}
+                    </span>
+                    <span onClick={onMutedToggle} >
+                        {!clipVidControls.muted ? <RiVolumeUpFill /> : <RiVolumeMuteFill />}
+                    </span>
 
                 </div>
                 <video ref={clipVid} autoPlay muted={clipVidControls.muted} src={vid}></video>
@@ -50,18 +49,16 @@ const Clips = () => {
                 </div>
                 <div className='reactionContainer'>
                     <div className='reactionIcon'>
-                        <HiHeart />
+                        <RiHeartFill />
                         <h5>20k</h5>
                     </div>
                     <div className='reactionIcon'>
-                        <HiChat />
+                        <RiChatQuoteFill />
                         <h5>20k</h5>
                     </div>
                     <div className='reactionIcon'>
-                        <HiArrowUp />
                     </div>
                     <div className='reactionIcon'>
-                        <HiArrowDown />
                     </div>
                 </div>
             </div>
